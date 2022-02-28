dd364c36a837e4743a8504b5185f7e26005da309
sovisrushain <sovisrushain@gmail.com>
2022-03-01 01:22:49 +0530

    useState hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [count, setCount] = useState(10)
+
+  const [{count1, count2}, setCount2] = useState({count1: 3, count2:4})
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+        <button onClick={() => setCount(count - 1)}>Decrement</button>
+        <h1>{count}</h1>
+      </div>
+      <hr/>
+      <div>
+        <button onClick={() => setCount2(currentState => ({...currentState, count1: currentState.count1 + 1}))}>Increment count1</button>
+        <h1>count1: {count1}</h1>
+        <h1>count2: {count2}</h1>
+      </div>
     </div>
   );
 }
