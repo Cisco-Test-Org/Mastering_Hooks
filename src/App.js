c4f290e5f0b122b3dc363760e843a3a95466fa43
sovisrushain <sovisrushain@gmail.com>
2022-03-01 10:32:29 +0530

    re-initialize

--- src/App.js
+++ src/App.js
@@ -3,23 +3,9 @@ import './App.css';
 
 function App() {
 
-  const [count, setCount] = useState(10)
-
-  const [{count1, count2}, setCount2] = useState({count1: 3, count2:4})
-
   return (
     <div className="App">
-      <div>
-        <button onClick={() => setCount(count + 1)}>Increment</button>
-        <button onClick={() => setCount(count - 1)}>Decrement</button>
-        <h1>{count}</h1>
-      </div>
-      <hr/>
-      <div>
-        <button onClick={() => setCount2(currentState => ({...currentState, count1: currentState.count1 + 1}))}>Increment count1</button>
-        <h1>count1: {count1}</h1>
-        <h1>count2: {count2}</h1>
-      </div>
+      <h1>Hey!</h1>
     </div>
   );
 }
